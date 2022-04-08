59a0dd898bce30bb1adf3aa56a2d39488249df06
Asyary Raihan <asyaryraihan@gmail.com>
2022-04-09 02:51:29 +0700

    More update

--- app.js
+++ app.js
@@ -94,50 +94,183 @@ client.on('message', async (msg) => {
   const args = lowerChat.trim().split(/ +/)
   const workbook = XLSX.readFile("test.xlsx")
   const isWriting = JSON.parse(fs.readFileSync("temp/from.json"))
+  const realThor = msg.author.replace('@c.us', '')
 
-  function errorDate() {
-    msg.reply("Invalid date format!")
-  }
-  function dateCompare(arr) {
-
+  // Kumpulan function
+  function errorOut(arg) {
+    switch(arg) {
+      case "date":
+        msg.reply("Invalid date format!")
+      break
+      case "input":
+        msg.reply("Invalid input!")
+      break
+      case "use":
+        msg.reply("Someone else is using this command. Please wait a moment!")
+      break
+    }
   }
   function init(thor) {
     fs.writeFileSync("temp/from.json", JSON.stringify(thor))
     fs.writeFileSync("temp/date.json", JSON.stringify(moment().tz("Israel").format("DD/MM/YY")))
     if (!fs.existsSync("database/" + thor)) {
       fs.mkdirSync("database/" + thor)
+      fs.writeFileSync("database/" + thor + "/master.json", "[]")
+      fs.writeFileSync("database/" + thor + "/date.json", "[]")
+    }
+  }
+  function checkInp(thor) {
+    if (thor == JSON.parse(fs.readFileSync("temp/from.json"))) {
+      return true
+    } else if (JSON.parse(fs.readFileSync("temp/from.json")) == "") {
+      msg.reply("No input! Use !start to start input.")
+      return false
+    } else {
+      errorOut("use")
+      return false
     }
   }
   function inp() {
     // All processing input is here
-    let thor = fs.readFileSync("temp/from.json")
-    let daDate = fs.readFileSync("temp/date.json")
-    let val = fs.readFileSync("temp/value.json")
-    // Compare dates
-    
+    let thor = JSON.parse(fs.readFileSync("temp/from.json"))
+    let daDate = JSON.parse(fs.readFileSync("temp/date.json"))
+    let val = JSON.parse(fs.readFileSync("temp/value.json"))
+    // let userExcel = thor.replace('@c.us', '')
+    let dateMaster = JSON.parse(fs.readFileSync("database/" + thor + "/date.json"))
+    let userMaster = JSON.parse(fs.readFileSync("database/" + thor + "/master.json"))
+
+    if (userMaster == "") {
+      for (let i = 0; i < Object.keys(val).length; i++) {
+        userMaster[i] = {}
+        userMaster[i]["List"] = Object.keys(val)[i]
+        userMaster[i][daDate] = val[Object.keys(val)[i]]
+      }
+      fs.writeFileSync("database/" + thor + "/master.json", JSON.stringify(userMaster))
+      dateMaster.push(daDate)
+      fs.writeFileSync("database/" + thor + "/date.json", JSON.stringify(dateMaster))
+      uploadExcel()
+    } else {
+      // Compare dates
+      for (let i = 0; i < dateMaster.length; i++) {
+        if (compareDate(daDate, dateMaster[i])) {
+          // Somewhere in the middle
+          //console.log(dateMaster[i])
+          dateMaster.splice(i, 0, daDate)
+          objWhereAt(userMaster, daDate, i)
+          break
+        } else if (i == dateMaster.length-1 && compareDate(dateMaster[i], daDate)) {
+          // At the end
+          dateMaster.splice(i+1, 0, daDate)
+          objWhereAt(userMaster, daDate, i+1)
+          break
+        } else if (i == dateMaster.length-1 && compareDate(dateMaster[0], daDate)) {
+          // At the beginning
+          dateMaster.splice(0, 0, daDate)
+          objWhereAt(userMaster, daDate, 0)
+          break
+        }
+      }
+      fs.writeFileSync("database/" + msg.author + "/date.json", JSON.stringify(dateMaster))
+      // Set object (master.json) for setExcel() req
+      // setExcel()
+    }
+  }
+  function objWhereAt(obj, deDate, where) {
+    let val = JSON.parse(fs.readFileSync("temp/value.json"))
+    let valKey = Object.keys(val)
+    let datoMaster = JSON.parse(fs.readFileSync("database/" + msg.author + "/date.json"))
+    var objVal = []
+    for (let i = 0; i < obj.length; i++) {
+      var objValTemp = [].concat.apply([], Object.values(obj[i]))
+      var objVal = objVal.concat(objValTemp)
+      obj[i] = Object.entries(obj[i])
+    }
+    for (let i = 0; i < obj.length; i++) {
+      if (val[obj[i][0][1]] != undefined) {
+        obj[i].splice(where+1, 0, [deDate, val[obj[i][0][1]]])
+      } else {
+        obj[i].splice(where+1, 0, [deDate, ""])
+      }
+    }
+    for (let i = 0; i < obj.length; i++) {
+      obj[i] = Object.fromEntries(obj[i])
+    }
+    for (let i = 0; i < valKey.length; i++) {
+      if (!objVal.includes(valKey[i])) {
+        let newObj = {}
+        newObj.List = valKey[i]
+        datoMaster.map(x => {
+          newObj[x] = ""
+        })
+        newObj[deDate] = val[valKey[i]]
+        //newObj[valKey[i]] = val[valKey[i]]
+        obj.push(newObj)
+      }
+    }
+    fs.writeFileSync("database/" + msg.author + "/master.json", JSON.stringify(obj))
   }
   function setExcel() {
     // All setup for the excel (coloring) is here
 
+    uploadExcel()
   }
   function uploadExcel() {
 
+    resetTemp()
+  }
+  function resetTemp() {
+    fs.writeFileSync("temp/from.json", "[]")
+    fs.writeFileSync("temp/date.json", "[]")
+    fs.writeFileSync("temp/value.json", "{}")
+  }
+  function inpReset() {
+    fs.writeFileSync("temp/date.json", JSON.stringify(moment().tz("Israel").format("DD/MM/YY")))
+    fs.writeFileSync("temp/value.json", "{}")
+  }
+  function isRed() {
+
+  }
+  function isYellow() {
+
+  }
+  function isGreen() {
+    
+  }
+  function compareDate(date1, date2) {	
+    return moment(date1, "DD/MM/YY").valueOf() < moment(date2, "DD/MM/YY").valueOf()
   }
 
   if (msg.author == JSON.parse(fs.readFileSync("temp/from.json"))) {
     try {
-      if (/^\d?\d\/\d?\d\/\d\d$/gm.test(msg.body)) {
+      let val = JSON.parse(fs.readFileSync("temp/value.json"))
+      if (/^\d\d\/\d\d\/\d\d$/gm.test(msg.body)) {
         let inputDate = msg.body
         let splitDate = inputDate.split("/")
         if (splitDate[0] <= 31 && splitDate[1] <= 12) {
           fs.writeFileSync("temp/date.json", JSON.stringify(msg.body))
           msg.reply("Date updated!")
         } else {
-          errorDate()
+          errorOut("date")
         }
       } else if (args[0] == "add") {
-        let val = fs.readFileSync("temp/value.json")
-        
+        // args[2] must exist, args[2] must be a number
+        if (/^(\+|\-)?\d+$/g.test(args[2])) {
+          val[args[1]] = args[2]
+          fs.writeFileSync("temp/value.json", JSON.stringify(val))
+          msg.reply("Label *" + args[1] + "* has been added with the value of *" + args[2] + "*!")
+        } else {
+          errorOut("input")
+        }
+      } else if (args[0] == "remove") {
+        if (/^[A-Za-z]+$/g.test(args[1]) && args.length == 2 && val[args[1]]) {
+          delete val[args[1]]
+          fs.writeFileSync("temp/value.json", JSON.stringify(val))
+          msg.reply("Input *" + args[1] + "* has been canceled!")
+        } else if (!val[args[1]]) {
+          msg.reply("Input *" + args[1] + "* does not exist!")
+        } else {
+          errorOut("input")
+        }
       }
     } catch (err) {
       console.log("[ERROR] " + err)
@@ -158,20 +291,18 @@ client.on('message', async (msg) => {
 
         case prefix + "start":
           if (JSON.parse(fs.readFileSync("temp/from.json")) != "") {
-            msg.reply("Someone else is using this command. Please wait a moment!")
+            errorOut("use")
           } else {
             msg.reply("Input process started!\n\nPlease input a date with dd/mm/yy format, else it'll be automatically set to today (" + moment().tz("Israel").format("DD/MM/YY") + ").")
-            client.sendMessage(msg.from, "Available list:\n")
-            init(msg.author)
+            if (fs.existsSync("database/" + msg.author)) {
+              client.sendMessage(msg.from, "Available list:\n")
+            }
+            await init(msg.author)
           }
         break
 
         case prefix + "debug":
-          let worksheets = {};
-          for (const sheetName of workbook.SheetNames) {
-            worksheets[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-          }
-          console.log(worksheets.Sheet1)
+          inp()
         break
 
         case prefix + "logout":
@@ -182,24 +313,42 @@ client.on('message', async (msg) => {
           })()
         break
 
+        case prefix + "reset":
+          if (checkInp(msg.author)) {
+            inpReset()
+            msg.reply("Value and date is reset!")
+          }
+        break
+
+        case prefix + "cancel":
+          if (checkInp(msg.author)) {
+            resetTemp()
+            msg.reply("Canceled!")
+          }
+        break
+
         case prefix + "test":
-          const jsonTest = fs.readFileSync("temp/temp.json")
+          // const jsonTest = JSON.parse(fs.readFileSync("temp/temp.json"))
+          // const iniWb = XLSX.utils.book_new()
+          // var iniWs = XLSX.utils.json_to_sheet(jsonTest)
+          // XLSX.utils.book_append_sheet(iniWb, iniWs, "Sheesh")
+          // XLSX.writeFile(iniWb, "Hm.xlsx")
+        break
+
+        case prefix + "ngetest":
+          const jsonTest = JSON.parse(fs.readFileSync("database/" + msg.author + "/master.json"))
           const iniWb = XLSX.utils.book_new()
           var iniWs = XLSX.utils.json_to_sheet(jsonTest)
           XLSX.utils.book_append_sheet(iniWb, iniWs, "Sheesh")
-          XLSX.writeFile(iniWb, "Hm.xlsx")
+          XLSX.writeFile(iniWb, "Hmm.xlsx")
         break
 
         case prefix + "finish":
-          if (msg.author == fs.readFileSync("temp/from.json")) {
+          if (checkInp(msg.author)) {
             msg.reply("Input succeeded! Upload engaging.")
             inp()
-            fs.writeFileSync("temp/from.json", "[]")
-            fs.writeFileSync("temp/date.json", "[]")
-          } else if (fs.readFileSync("temp/from.json") == "") {
-            msg.reply("No input! Use !start to start input.")
           } else {
-            msg.reply("Someone else is using this command. Please wait a moment!")
+            errorOut("use")
           }
         break
 
@@ -209,6 +358,7 @@ client.on('message', async (msg) => {
     }
   } catch (err) {
     console.log('[ERROR] ' + err)
+    msg.reply("[ERROR] " + err)
   }
 });
 
